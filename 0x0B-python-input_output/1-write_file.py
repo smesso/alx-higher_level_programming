f37921feb30e218040754477c372888b8bfe47d4
Ismail <ismailessoudaigui@gmail.com>
2024-02-06 19:12:22 +0100

    python-input/output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,9 +2,8 @@
 """Defines a file-writing function."""
 
 
-def write_file(filename: "", text: ""):
-    """
-    Write a string to a UTF8 text file.
+def write_file(filename:"", text:""):
+    """Write a string to a UTF8 text file.
 
     Args:
         filename (str): The name of the file to write.
@@ -15,4 +14,3 @@ def write_file(filename: "", text: ""):
     """
     with open(filename, "w", encoding="utf-8") as file:
         return file.write(text)
-
